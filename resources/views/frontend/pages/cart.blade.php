76721091ce9614642c73489cbc76ea4f2d1a6610
nhanphu09 <119411929+nhanphu09@users.noreply.github.com>
2025-09-19 01:46:11 +0000

    new committed changes

--- resources/views/frontend/pages/cart.blade.php
+++ resources/views/frontend/pages/cart.blade.php
@@ -67,7 +67,7 @@
 												</div>
 												<!--/ End Input Order -->
 											</td>
-											<td class="total-amount cart_single_price" data-title="Total"><span class="money">{{number_format($cart['amount']),0}}đ</span></td>
+											<td class="total-amount cart_single_price" data-title="Total"><span class="money">{{number_format($cart['amount'])0}}đ</span></td>
 
 											<td class="action" data-title="Remove"><a href="{{route('cart-delete',$cart->id)}}"><i class="ti-trash remove-icon"></i></a></td>
 										</tr>
